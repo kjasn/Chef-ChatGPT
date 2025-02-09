16eade1a52a9230d7a319e0330a2c78113737aac
Kjasn <iz.kjasn@qq.com>
2025-02-09 13:31:23 +0800

    feat: add speed insight

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 function App() {
     return (
         <>
             <Header />
             <Main />
+            <SpeedInsights />
         </>
     );
 }
